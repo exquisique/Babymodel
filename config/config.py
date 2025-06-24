4a8cb890535585666bb081ccf813377cce758e3a
Exquisique <iampanky01@gmail.com>
2025-06-24 23:36:47 +0530

    Added config and model modules for Babymodel

---
+++ config/config.py
@@ -0,0 +1,25 @@
+# config/config.py
+
+class GPTConfig:
+    vocab_size = 50257
+    block_size = 128
+    n_layer = 6
+    n_head = 6
+    n_embd = 384
+    dropout = 0.1
+    bias = True
+
+class TrainConfig:
+    batch_size = 32
+    max_iters = 20000
+    learning_rate = 1e-4
+    warmup_steps = 1000
+    min_lr = 5e-4
+    eval_iters = 500
+    gradient_accumulation_steps = 32
+    dtype = 'bfloat16'
+    best_model_params_path = "best_model_params.pt"
+    seed = 42
+
+class DatasetConfig:
+    dataset_name = "roneneldan/TinyStories"
